b274c4f3cfdcca6f4e700e1974b63ff928bfa16b
chris baurkot <chrisbaurkot@chriss-mbp-3.myfiosgateway.com>
2023-11-09 19:14:55 -0500

    Added pseudocode for todo list

---
+++ todos.py
@@ -0,0 +1,7 @@
+# Create application that asks for user input from command line to create a todo list
+# 1. Create main function that asks users what they want to do
+    # Options: View All Todos, Add a Todo, Complete a Todo, Remove a Todo, View Completed Todos, Exit program
+# 2. Create methods for each of the following options that prints out what the user selected
+# 3. Create a database with the Todos table to store Todos
+# 4. Update methods so that they are creating/deleting/updating user Todos
+
